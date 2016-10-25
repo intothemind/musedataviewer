b6c853610bbec72263f31e7a8b08c7ac434bf1f1
chrischne <chrischne@gmail.com>
2016-10-25 20:01:36 +0200

    bug in jawView

--- jawView.js
+++ jawView.js
@@ -12,7 +12,7 @@ function jawView(model, title, description) {
 		//console.log('title: ' + my.getTitle());
 		//console.log('description: ' + my.getDescription());
 
-		my.renderCommon();
+		//my.renderCommon();
 
 		
 		console.log(model.jaw);
